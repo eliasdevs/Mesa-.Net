11f34a8383bd4d1208cbe6e4d986d289859a079e
emunguia <emunguia@bitworks.com.sv>
2023-08-05 16:18:51 -0600

    Tipo juego en la request

---
+++ Mesa.Blackjack.Data/Migrations/20230805221755_TIPOJUEGO.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mesa.Blackjack.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TIPOJUEGO : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TipoJuego",
+                table: "GameRequest",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TipoJuego",
+                table: "GameRequest");
+        }
+    }
+}
