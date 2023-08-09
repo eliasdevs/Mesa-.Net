d15eef9fe098ed50fc5f809c461fb985f75c990c
emunguia <emunguia@bitworks.com.sv>
2023-08-09 14:20:11 -0600

    modificar el store poner mano que sea toda la info que devuelve la Api

--- Mesa.Juegos.State/States/BlackJackSore.cs
+++ Mesa.Juegos.State/States/BlackJackSore.cs
@@ -1,5 +1,6 @@
 ﻿using Fluxor;
 using Mesa_SV.BlackJack.Dtos.Output;
+using Mesa_SV.VoDeJuegos;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -16,7 +17,7 @@ namespace Mesa.Juegos.State.States
     /// <param name="Request">La solicitud que se esta ligado</param>
     /// <param name="Loader">Se pone en true algunas propiedades cuando el proceso lo requiere</param>
     /// <param name="BlackJackRequests">Esta Representa todas Las solicitudes que existen, se debe actualizar cada que se creen, cierren o finalicen</param>
-    public record BlackJackSore(ImmutableList<CardOutput> Mano, 
+    public record BlackJackSore(ManoJugadorVo? Mano, 
         GameRequestBackJackOutput? Request,
         BlackjackStartOutput? BlackjackInfo,
         BlackJackLoaders Loader,
@@ -40,7 +41,7 @@ namespace Mesa.Juegos.State.States
 
         protected override BlackJackSore GetInitialState()
         {
-            return new BlackJackSore(ImmutableList.Create<CardOutput>()
+            return new BlackJackSore(null
                 , null, null, 
                 new BlackJackLoaders(false, false, false, false),
                 ImmutableList.Create<GameRequestBackJackOutput>());
