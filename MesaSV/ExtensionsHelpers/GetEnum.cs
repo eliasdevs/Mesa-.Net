de64fba0c95e4fe12754cccd3233a449818e3895
emunguia <emunguia@bitworks.com.sv>
2023-09-13 12:40:16 -0600

    agregar componente para mostrar las cartas

---
+++ MesaSV/ExtensionsHelpers/GetEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mesa_SV.ExtensionsHelpers
+{
+    public static class GetEnum
+    {
+        public static int GetValueInt(this TypeCard valueEnum)
+        {
+            return Convert.ToInt32(valueEnum);
+        }
+    }
+}
